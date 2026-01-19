e0842fc9465b048a65fdecfdbe9ca11684d0ee74
hadinajem52 <132664445+hadinajem52@users.noreply.github.com>
2026-01-19 13:24:10 +0200

    image fixes

--- src/screens/ReportIncidentScreen.js
+++ src/screens/ReportIncidentScreen.js
@@ -367,7 +367,7 @@ const ReportIncidentScreen = ({ navigation, route }) => {
       }
 
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        mediaTypes: ['images'],
         allowsEditing: true,
         aspect: [4, 3],
         quality: 0.8,
